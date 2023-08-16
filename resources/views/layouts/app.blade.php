6f1beb7a63decbe8263b84957432979c2759eeb3
Rizky Dhani Ismail <rizkydhani15@gmail.com>
2023-08-16 19:16:58 +0800

    create dashboard page and breakdown navs and main into components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ $title }} - Omni Hotelier</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
+
 <body>
 
+    <div class="wrapper">
+        @include('layouts.components.navs.sidebar')
+
+        <div class="main">
+            @include('layouts.components.navs.header')
+
+            <main class="content">
+                @yield('content')
+            </main>
+
+            @include('layouts.components.footer')
+        </div>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
